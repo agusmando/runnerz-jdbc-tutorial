18772f410d02a56fc312a94e2f0e1fc8354bf218
Agustín Mandolesi <agustinm@itcsoluciones.com>
2024-09-24 13:20:07 -0300

    13:20 trabajo

--- src/main/java/dev/agus/runnerz/run/RunRepository.java
+++ src/main/java/dev/agus/runnerz/run/RunRepository.java
@@ -1,12 +1,15 @@
 package dev.agus.runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+@Repository
 public class RunRepository {
 
     private List<Run> runs = new ArrayList<>();
@@ -18,6 +21,17 @@ public class RunRepository {
     @PostConstruct
     private void init() {
         runs.add(new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(50, ChronoUnit.MINUTES), 5, Location.OUTDOORS));
-        runs.add(new Run(1, "Evening Run", LocalDateTime.now(), LocalDateTime.now().plus(50, ChronoUnit.MINUTES), 5, Location.OUTDOORS));
+        runs.add(new Run(2, "Evening Run", LocalDateTime.now(), LocalDateTime.now().plus(50, ChronoUnit.MINUTES), 5, Location.INDOORS));
+    }
+
+
+    Optional<Run> findById(Integer id) {
+        return runs.stream()
+                .filter(run -> run.id() == id)
+                .findFirst();
+    }
+
+    void create(Run run) {
+        runs.add(run);
     }
 }
